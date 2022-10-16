4ff464da71bfd423fe6ba40da355b2a3f735889c
masukomi <masukomi@masukomi.org>
2022-10-16 16:49:15 -0400

    updated triggers to accomodate tags

--- resources/database.db.sql
+++ resources/database.db.sql
@@ -7,6 +7,35 @@ CREATE TABLE IF NOT EXISTS "cheats" (
 	FOREIGN KEY("command_id") REFERENCES "commands"("id"),
 	PRIMARY KEY("id" AUTOINCREMENT)
 );
+CREATE TABLE IF NOT EXISTS "clu_metadata" (
+	"key"	TEXT NOT NULL,
+	"value"	TEXT NOT NULL,
+	PRIMARY KEY("key")
+);
+CREATE TABLE IF NOT EXISTS "commands" (
+	"id"	INTEGER NOT NULL,
+	"name"	TEXT NOT NULL UNIQUE,
+	"description"	TEXT,
+	"usage_command"	TEXT,
+	"fallback_usage"	TEXT,
+	"location"	TEXT,
+	"type"	TEXT,
+	"language"	TEXT,
+	"source_url"	TEXT,
+	"source_repo_url"	TEXT,
+	"asciicast_url"	TEXT,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+CREATE TABLE IF NOT EXISTS "tags" (
+	"id"	INTEGER NOT NULL,
+	"tag"	TEXT NOT NULL UNIQUE,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+CREATE TABLE IF NOT EXISTS "commands_tags" (
+	"command_id"	INTEGER NOT NULL,
+	"tag_id"	INTEGER NOT NULL,
+	PRIMARY KEY("tag_id","command_id")
+);
 CREATE VIRTUAL TABLE commands_fts USING fts5(
 	id,
 	name,
@@ -38,43 +67,15 @@ CREATE TABLE IF NOT EXISTS "commands_fts_config" (
 	"v"	,
 	PRIMARY KEY("k")
 ) WITHOUT ROWID;
-CREATE TABLE IF NOT EXISTS "clu_metadata" (
-	"key"	TEXT NOT NULL,
-	"value"	TEXT NOT NULL,
-	PRIMARY KEY("key")
-);
-CREATE TABLE IF NOT EXISTS "commands" (
-	"id"	INTEGER NOT NULL,
-	"name"	TEXT NOT NULL UNIQUE,
-	"description"	TEXT,
-	"usage_command"	TEXT,
-	"fallback_usage"	TEXT,
-	"location"	TEXT,
-	"type"	TEXT,
-	"language"	TEXT,
-	"source_url"	TEXT,
-	"source_repo_url"	TEXT,
-	"asciicast_url"	TEXT,
-	PRIMARY KEY("id" AUTOINCREMENT)
-);
-CREATE TABLE IF NOT EXISTS "tags" (
-	"id"	INTEGER NOT NULL,
-	"tag"	TEXT NOT NULL UNIQUE,
-	PRIMARY KEY("id" AUTOINCREMENT)
-);
-CREATE TABLE IF NOT EXISTS "commands_tags" (
-	"command_id"	INTEGER NOT NULL,
-	"tag_id"	INTEGER NOT NULL,
-	PRIMARY KEY("tag_id","command_id")
-);
+INSERT INTO "clu_metadata" ("key","value") VALUES ('db_version','2.0.0');
 INSERT INTO "commands_fts_data" ("id","block") VALUES (1,'');
 INSERT INTO "commands_fts_data" ("id","block") VALUES (10,X'00000000000000');
 INSERT INTO "commands_fts_config" ("k","v") VALUES ('version',4);
-INSERT INTO "clu_metadata" ("key","value") VALUES ('db_version','2.0.0');
 CREATE TRIGGER commands_fts_insert AFTER INSERT ON commands
 BEGIN
     INSERT INTO commands_fts (rowid, name, description, language, tags)
-	VALUES (new.rowid, new.name, new.description, new.language
+
+	VALUES (new.rowid, new.name, new.description, new.language,
 			(
 				SELECT GROUP_CONCAT(t.tag)
 				FROM tags t
@@ -99,7 +100,7 @@ BEGIN
     INSERT INTO commands_fts
 	(rowid, name, description, language, tags)
 	VALUES
-	(new.rowid, new.name, new.description, new.language, tags
+	(new.rowid, new.name, new.description, new.language,
 			(
 				SELECT GROUP_CONCAT(t.tag)
 				FROM tags t

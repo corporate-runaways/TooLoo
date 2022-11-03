4287887fe37551ee36a326db9be803a41941aab2
masukomi <masukomi@masukomi.org>
2022-11-03 11:25:19 -0400

    added tags_fts table & triggers

--- resources/database.db.sql
+++ resources/database.db.sql
@@ -7,6 +7,24 @@ CREATE TABLE IF NOT EXISTS "cheats" (
 	FOREIGN KEY("command_id") REFERENCES "commands"("id"),
 	PRIMARY KEY("id" AUTOINCREMENT)
 );
+CREATE VIRTUAL TABLE commands_fts USING fts5(
+	id,
+	name,
+	description,
+	language,
+	content=commands,
+	content_rowid=id,
+	tokenize=porter
+);
+CREATE VIRTUAL TABLE tags_fts USING fts5(
+	id,
+	tag,
+	content=tags,
+	content_rowid=id,
+	tokenize=porter
+);
+
+
 CREATE TABLE IF NOT EXISTS "clu_metadata" (
 	"key"	TEXT NOT NULL,
 	"value"	TEXT NOT NULL,
@@ -36,94 +54,50 @@ CREATE TABLE IF NOT EXISTS "commands_tags" (
 	"tag_id"	INTEGER NOT NULL,
 	PRIMARY KEY("tag_id","command_id")
 );
-CREATE VIRTUAL TABLE commands_fts USING fts5(
-	id,
-	name,
-	description,
-	language,
-	all_tags,
-	content=commands,
-	content_rowid=id,
-	tokenize=porter
-);
-CREATE TABLE IF NOT EXISTS "commands_fts_data" (
-	"id"	INTEGER,
-	"block"	BLOB,
-	PRIMARY KEY("id")
-);
-CREATE TABLE IF NOT EXISTS "commands_fts_idx" (
-	"segid"	,
-	"term"	,
-	"pgno"	,
-	PRIMARY KEY("segid","term")
-) WITHOUT ROWID;
-CREATE TABLE IF NOT EXISTS "commands_fts_docsize" (
-	"id"	INTEGER,
-	"sz"	BLOB,
-	PRIMARY KEY("id")
-);
-CREATE TABLE IF NOT EXISTS "commands_fts_config" (
-	"k"	,
-	"v"	,
-	PRIMARY KEY("k")
-) WITHOUT ROWID;
 INSERT INTO "clu_metadata" ("key","value") VALUES ('db_version','2.0.0');
-
+-- Commands FTS triggers
 CREATE TRIGGER commands_fts_insert AFTER INSERT ON commands
 BEGIN
-    INSERT INTO commands_fts (rowid, name, description, language, all_tags)
-
-	VALUES (new.rowid, new.name, new.description, new.language,
-			(
-				SELECT GROUP_CONCAT(t.tag)
-				FROM tags t
-				INNER JOIN commands_tags ct on ct.tag_id = t.id
-				INNER JOIN commands c on ct.command_id = c.id
-				WHERE c.rowid = new.rowid
-			)
-	);
+    INSERT INTO commands_fts (rowid, name, description, language) VALUES (new.rowid, new.name, new.description, new.language);
 END;
-
 CREATE TRIGGER commands_fts_delete AFTER DELETE ON commands
 BEGIN
     INSERT INTO commands_fts
-	(commands_fts, rowid, name, description, language, all_tags)
+	(commands_fts, rowid, name, description, language)
 	VALUES
-	('delete', old.rowid, old.name, old.description, old.language, old.all_tags);
+	('delete', old.rowid, old.name, old.description, old.language);
 END;
-
--- CREATE TRIGGER commands_fts_delete AFTER DELETE ON commands
--- BEGIN
---     INSERT INTO commands_fts
--- 	(commands_fts, rowid, name, description, language)
--- 	VALUES
--- 	('delete', old.rowid, old.name, old.description, old.language);
--- END;
-
--- CREATE TRIGGER commands_fts_delete AFTER DELETE ON commands
--- BEGIN
---     INSERT INTO commands_fts
--- 	(commands_fts, rowid, name, description, language, tags )
--- 	VALUES
--- 	('delete', old.rowid, old.name, old.description, old.language, null);
--- END;
-
--- CREATE TRIGGER commands_fts_update AFTER UPDATE ON commands
--- BEGIN
---     INSERT INTO commands_fts
--- 	(commands_fts, rowid, name, description, language, tags)
--- 	VALUES ('delete', old.rowid, old.name, old.description, old.language, old.tags);
---     INSERT INTO commands_fts
--- 	(rowid, name, description, language, tags)
--- 	VALUES
--- 	(new.rowid, new.name, new.description, new.language,
--- 			(
--- 				SELECT GROUP_CONCAT(t.tag)
--- 				FROM tags t
--- 				INNER JOIN commands_tags ct on ct.tag_id = t.id
--- 				INNER JOIN commands c on ct.command_id = c.id
--- 				WHERE c.rowid = new.rowid
--- 			)
--- 	);
--- END;
+CREATE TRIGGER commands_fts_update AFTER UPDATE ON commands
+BEGIN
+    INSERT INTO commands_fts
+	(commands_fts, rowid, name, description, language)
+	VALUES ('delete', old.rowid, old.name, old.description, old.language);
+    INSERT INTO commands_fts
+	(rowid, name, description, language)
+	VALUES
+	(new.rowid, new.name, new.description, new.language);
+END;
+COMMIT;
+-- Tags FTS triggers
+CREATE TRIGGER tags_fts_insert AFTER INSERT ON tags
+BEGIN
+    INSERT INTO tags_fts (rowid, tag) VALUES (new.rowid, new.tag);
+END;
+CREATE TRIGGER tags_fts_delete AFTER DELETE ON tags
+BEGIN
+    INSERT INTO tags_fts
+	(tags_fts, rowid, tag)
+	VALUES
+	('delete', old.rowid, old.tag);
+END;
+CREATE TRIGGER tags_fts_update AFTER UPDATE ON tags
+BEGIN
+    INSERT INTO tags_fts
+	(tags_fts, rowid, tag)
+	VALUES ('delete', old.rowid, old.tag);
+    INSERT INTO tags_fts
+	(rowid, tag)
+	VALUES
+	(new.rowid, new.tag);
+END;
 COMMIT;

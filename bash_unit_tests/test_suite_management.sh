040cd18fcaff367d3d08f375a2ac64689dc8f23b
masukomi <masukomi@masukomi.org>
2022-11-05 17:23:34 -0400

    more tests

--- bash_unit_tests/test_suite_management.sh
+++ bash_unit_tests/test_suite_management.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env sh
 
 setup_suite() {
-	XDG_DATA_HOME=$(pwd)"/TEST_XDG_DATA_HOME";
+	XDG_DATA_HOME=$(pwd)"/bash_unit_tests/TEST_XDG_DATA_HOME";
 	rm -rf $XDG_DATA_HOME 2>&1 > /dev/null
 	mkdir -p $XDG_DATA_HOME
+	TEST_DATA_DIR=$(pwd)"/bash_unit_tests/test_data"
+
 
 }
 

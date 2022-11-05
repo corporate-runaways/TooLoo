5efe45e8f046db4d4ab6df9e0b5d0a830646acc0
masukomi <masukomi@masukomi.org>
2022-11-05 18:40:09 -0400

    more tests

--- bash_unit_tests/clu_test.sh
+++ bash_unit_tests/clu_test.sh
@@ -13,43 +13,66 @@ fi
 
 
 
-test_1_hyphen_v () {
+test_01_hyphen_v () {
   #assert_equals expected actual message
-  clu_hyphen_v=$(raku -I lib clu -v 2>&1 | head -n2 | tail -n1)
+  clu_hyphen_v=$(XDG_DATA_HOME=$XDG_DATA_HOME raku -I lib clu -v 2>&1 | head -n2 | tail -n1)
   assert_equals "$clu_hyphen_v" "  clu -V|--version[=Any] [--verbose[=Any]]" \
 	  "should provide usage for clu -v"
 
-  assert_status_code 2 `raku -I lib clu -v > /dev/null 2>&1`
+  assert_status_code 2 "raku -I lib clu -v > /dev/null 2>&1"
 }
 
-test_2_hyphen_hyphen_version () {
-  clu_hyphen_v=$(raku -I lib clu --version 2>&1 | sed -e "s/ running.*//")
-  assert_equals "$clu_hyphen_v" "clu - provided by  ," \
+test_02_confirm_no_db(){
+	assert "test ! -e $DB_LOCATION"
+}
+
+test_03_list_empty(){
+	no_content_output=$(XDG_DATA_HOME=$XDG_DATA_HOME raku -I lib clu list)
+	assert_equals "Your database is currently empty." "$no_content_output"
+}
+
+test_04_hyphen_hyphen_version () {
+  clu_hyphen_v=$(XDG_DATA_HOME=$XDG_DATA_HOME raku -I lib clu --version 2>&1 | sed -e "s/ running.*//")
+  assert_equals "clu - provided by  ," "$clu_hyphen_v"\
 	  "should provide usage for clu --version"
 
   assert_status_code 2 `raku -I lib clu --version > /dev/null 2>&1`
 }
 
-test_3_add_new() {
+test_05_add_new() {
 	file_path=$TEST_DATA_DIR"/raku_test_no_demo.meta.toml"
-	add_new_output=$(raku -I lib clu add $file_path | sed -e "s/ \/.*//")
+	add_new_output=$(XDG_DATA_HOME=$XDG_DATA_HOME raku -I lib clu add $file_path | sed -e "s/ \/.*//")
 	assert_equals "$add_new_output" "Successfully ingested"
 }
 
-test_4_add_existing() {
+test_06_confirm_db(){
+	assert "test -e $DB_LOCATION"
+}
+
+test_07_confirm_data() {
+	commands_count=$(sqlite3 $DB_LOCATION 'select count(*) from commands');
+	tags_count=$(sqlite3 $DB_LOCATION 'select count(*) from tags');
+	commands_tags_count=$(sqlite3 $DB_LOCATION 'select count(*) from commands_tags');
+	assert_equals "1" "$commands_count"
+	assert_equals "2" "$tags_count"
+	assert_equals "2" "$commands_tags_count"
+
+}
+
+test_08_add_existing() {
 	file_path=$TEST_DATA_DIR"/raku_test_no_demo.meta.toml"
-	add_existing_output=$(raku -I lib clu add $file_path | sed -e "s/ \/.*//")
+	add_existing_output=$(XDG_DATA_HOME=$XDG_DATA_HOME raku -I lib clu add $file_path | sed -e "s/ \/.*//")
 	assert_equals "$add_existing_output" "Successfully ingested"
 }
 
-test_5_update() {
+test_09_update() {
 	file_path=$TEST_DATA_DIR"/raku_test_no_demo.meta.toml"
-	update_output=$(raku -I lib clu update $file_path | sed -e "s/ \/.*//")
+	update_output=$(XDG_DATA_HOME=$XDG_DATA_HOME raku -I lib clu update $file_path | sed -e "s/ \/.*//")
 	assert_equals "$update_output" "Successfully ingested"
 }
 
-test_6_add_asciicast() {
+test_10_add_asciicast() {
 	file_path=$TEST_DATA_DIR"/raku_test_no_demo.cast"
-	add_cast_output=$(raku -I lib clu update $file_path | sed -e "s/ \/.*//")
+	add_cast_output=$(XDG_DATA_HOME=$XDG_DATA_HOME raku -I lib clu update $file_path | sed -e "s/ \/.*//")
 	assert_equals "$add_cast_output" "Successfully ingested"
 }
--- bash_unit_tests/test_suite_management.sh
+++ bash_unit_tests/test_suite_management.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env sh
 
 setup_suite() {
+
 	XDG_DATA_HOME=$(pwd)"/bash_unit_tests/TEST_XDG_DATA_HOME";
+	echo ""
+	echo "XDG_DATA_HOME: $XDG_DATA_HOME"
 	rm -rf $XDG_DATA_HOME 2>&1 > /dev/null
 	mkdir -p $XDG_DATA_HOME
 	TEST_DATA_DIR=$(pwd)"/bash_unit_tests/test_data"
-
+	DB_LOCATION=$XDG_DATA_HOME"/clu/database.db"
 
 }
 
 teardown_suite() {
+	#rm -rf $XDG_DATA_HOME
 	echo "DONE";
 }
